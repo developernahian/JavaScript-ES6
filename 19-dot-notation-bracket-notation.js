bcd49c797d6ce3e267bfcc5f7c46e26ea074f830
developernahian <developer.nahian@gmail.com>
2024-10-01 19:03:24 +0600

    update

---
+++ 19-dot-notation-bracket-notation.js
@@ -0,0 +1,26 @@
+// sob jaigai dot use kora jaina. example e 3 ke kod notation e use kora jaina. so etake bracket notation e sue korte hobe
+
+const person = {
+    name: 'kodom ali', 
+    job: 'badam khai',
+    3:'third',
+}
+//'job' re variable e rakhlam
+const prop = 'job'
+
+// dot notation
+console.log(person.job)
+
+// bracket notation
+// bracket notation "" ba '' er moddhe rakhte hoy
+console.log(person['job'])
+
+// bracket notation "" ba '' er moddhe rakhte hoy. tobe variable er moddhe rakhle quotation lage na
+console.log(person[prop])
+
+
+// console.log(person.3)//error
+console.log(person['3'])//this is correct
+
+
+
