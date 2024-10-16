611a9d23cd0f7e6483eab8dd8a0397dfeb333ab8
developernahian <developer.nahian@gmail.com>
2024-10-16 17:27:14 +0600

    update

--- 19-dot-notation-bracket-notation.js
+++ 19-dot-notation-bracket-notation.js
@@ -1,11 +1,14 @@
 // sob jaigai dot use kora jaina. example e 3 ke dod notation e use kora jaina. so etake bracket notation e sue korte hobe
 // abar 'hobby' ke dot notation diye access kora jabe na karon '' quotation ache. so ekakeo bracket notation diye access korte hobe
+//key te jodi hifen thake  my-name, tahole bracket notation use korte hobe
 
 const person = {
     name: 'kodom ali', 
     job: 'badam khai',
     3:'third',
     'hobby':'singing',
+    'last-name': 'khan',
+    'final-last-name': 'sardar'
 }
 //'job' re variable e rakhlam
 const prop = 'job'
@@ -26,6 +29,10 @@ console.log(person['3'])//this is correct
 
 //'' thakle must bracket notation use korte hobe
 console.log(person['hobby'])
+
+// key te jodi hyfen thake tahole must bracket notation use korte hobe
+console.log(person['last-name'])
+console.log(person['final-last-name'])
 
 
 
